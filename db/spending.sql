af208a8d2d9d47e758576fce5dfe19d9d41fc524
Conor <cwilson0005@hotmail.co.uk>
2019-01-18 12:57:40 +0000

    Added sql code for database tables

---
+++ db/spending.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS merchants;
+DROP TABLE IF EXISTS accounts;
+
+CREATE TABLE tags (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE merchants (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE accounts (
+  id SERIAL8 PRIMARY KEY,
+  first_name VARCHAR(255),
+  sur_name VARCHAR(255),
+  budget INT8
+);
+
+CREATE TABLE transactions (
+  id SERIAL8 PRIMARY KEY,
+  merchant_id INT8 REFERENCES merchants(id),
+  tag_id INT8 REFERENCES tags(id),
+  amount INT8,
+  transaction_date DATETIME2
+);
