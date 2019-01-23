2343e4dd1a0669ec6b54cef757b5bf4a8fbd2ab6
Conor <cwilson0005@hotmail.co.uk>
2019-01-23 13:55:05 +0000

    Tag and Merchant tests passed

--- models/specs/tag_spec.rb
+++ models/specs/tag_spec.rb
@@ -3,6 +3,17 @@ require_relative("../tag")
 
 class TestTag < MiniTest::Test
 
+  def setup
+    options = {
+      'name' => 'groceries'
+    }
 
+    @tag = Tag.new(options)
+  end
+
+  def test_name()
+    result = @tag.name()
+    assert_equal("groceries", result)
+  end
 
 end
