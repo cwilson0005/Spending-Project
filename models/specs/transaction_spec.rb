b9aa889304217a2c3f3444ee93b2afd4a5534c01
Conor <cwilson0005@hotmail.co.uk>
2019-01-23 13:57:12 +0000

    All tests passed

--- models/specs/transaction_spec.rb
+++ models/specs/transaction_spec.rb
@@ -3,6 +3,17 @@ require_relative("../transaction")
 
 class TestTransaction < MiniTest::Test
 
+  def setup
+    options = {
+      'amount' => '25.00'
+    }
 
+    @transaction = Transaction.new(options)
+  end
+
+  def test_amount()
+    result = @transaction.amount()
+    assert_equal(25.00, result)
+  end
 
 end
