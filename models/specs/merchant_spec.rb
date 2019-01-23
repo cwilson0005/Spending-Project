d01ad2887a3ca1ff617f9c56531c2d3aebca5d89
Conor <cwilson0005@hotmail.co.uk>
2019-01-23 13:54:05 +0000

    Account tests passed

--- models/specs/merchant_spec.rb
+++ models/specs/merchant_spec.rb
@@ -3,6 +3,17 @@ require_relative("../merchant")
 
 class TestMerchant < MiniTest::Test
 
+  def setup
+    options = {
+      'name' => 'Tesco'
+    }
 
+    @merchant = Merchant.new(options)
+  end
+
+  def test_name()
+    result = @merchant.name()
+    assert_equal("Tesco", result)
+  end
 
 end
